27094bda6fec3bc2a4f5ad4f882a5041b9dec3bc
mahdi_8165 <mahdi_8165@yahoo.com>
2018-08-08 17:35:55 +0430

    change file

---
+++ tx/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,45 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'tx_project' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX5             /* CMSIS-RTOS Keil RTX5 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+#define RTE_DEVICE_HAL_ADC
+#define RTE_DEVICE_HAL_CAN
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_CRC
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+#define RTE_RV_GENWAIT                     /* RTOS Validation - GenWait test enabled */
+#define RTE_RV_MAILQUEUE                   /* RTOS Validation - MailQueue test enabled */
+#define RTE_RV_MEMORYPOOL                  /* RTOS Validation - MemoryPool test enabled */
+#define RTE_RV_MSGQUEUE                    /* RTOS Validation - MsgQueue test enabled */
+#define RTE_RV_MUTEX                       /* RTOS Validation - Mutex test enabled */
+#define RTE_RV_SEMAPHORE                   /* RTOS Validation - Semaphore test enabled */
+#define RTE_RV_SIGNAL                      /* RTOS Validation - Signal test enabled */
+#define RTE_RV_THREAD                      /* RTOS Validation - Thread test enabled */
+#define RTE_RV_TIMER                       /* RTOS Validation - Timer test enabled */
+#define RTE_RV_WAITFUNC                    /* RTOS Validation - WaitFunc test enabled */
+
+#endif /* RTE_COMPONENTS_H */
